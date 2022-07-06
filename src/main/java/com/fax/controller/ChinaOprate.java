6d4f476122fdc9ed206686aa58d4f36c98e216d0
SuperAxFax <2787086427@qq.com>
2022-07-06 21:06:10 +0800

    完成对疫情数据的表格输出。

--- src/main/java/com/fax/controller/ChinaOprate.java
+++ src/main/java/com/fax/controller/ChinaOprate.java
@@ -6,7 +6,10 @@ import com.fax.vo.DataView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/china")
@@ -37,4 +40,6 @@ public class ChinaOprate {
         }
         return dataView;
     }
+
+
 }
--- src/main/java/com/fax/controller/ExcelUploadController.java
+++ src/main/java/com/fax/controller/ExcelUploadController.java
@@ -4,6 +4,7 @@ import com.fax.entity.NocvData;
 import com.fax.service.IndexService;
 import com.fax.vo.DataView;
 import com.fax.vo.NocvDataVo;
+import com.sun.deploy.net.HttpResponse;
 import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -18,7 +19,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,5 +56,42 @@ public class ExcelUploadController {
         System.out.println(dataView);
         System.out.println(list);
         return dataView;
+    }
+
+    /**导出中国疫情数据
+     * 步骤：
+     *    1：查询数据库
+     *    2：将查询到的数据封装成excel表格
+     *    3：建立输出流，导出数据(先拼接输出文件的名字，，后缀名，格式)
+     *    4：关闭流
+     */
+    @RequestMapping("/dooutput")
+    @ResponseBody
+    public void doOutput(HttpServletResponse response) throws Exception {
+        //1：查询数据库
+        List<NocvData> list = indexService.list();
+        //2：将查询到的数据封装成excel表格
+        XSSFWorkbook sheets = new XSSFWorkbook();
+        XSSFSheet sheet = sheets.createSheet("中国疫情数据表");
+        XSSFRow row = sheet.createRow(0);
+        row.createCell(0).setCellValue("城市名称");
+        row.createCell(1).setCellValue("确诊数量");
+        for (NocvData nocvData : list) {
+            XSSFRow sheetRow = sheet.createRow(sheet.getLastRowNum() + 1);
+            sheetRow.createCell(0).setCellValue(nocvData.getName());
+            sheetRow.createCell(1).setCellValue(nocvData.getValue());
+        }
+           //..3：调整reponse的页面格式，不然会乱码
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/octet-stream;chartset=utf8");
+        response.setHeader("Content-Disposition","attachment;filename="+ new String("中国疫情数据表".getBytes(),"iso-8859-1")+".xlsx");
+        //3：建立输出流，导出数据
+        OutputStream os = null;
+        os = response.getOutputStream();
+        sheets.write(os);
+        os.flush();
+
+        //4：关闭输出流
+        os.close();
     }
 }
